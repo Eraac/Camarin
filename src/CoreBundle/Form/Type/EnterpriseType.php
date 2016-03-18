bc972582c19bee6493a513947c536b4545f2013d
Eraac <kevin@labesse.me>
2016-03-18 19:36:04 +0100

    translate label

--- src/CoreBundle/Form/Type/EnterpriseType.php
+++ src/CoreBundle/Form/Type/EnterpriseType.php
@@ -4,6 +4,7 @@ namespace CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EnterpriseType extends AbstractType
@@ -15,8 +16,8 @@ class EnterpriseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('contact')
+            ->add('name', TextType::class, ['label' => 'core.form.enterprise.name'])
+            ->add('contact', TextType::class, ['label' => 'core.form.enterprise.contact'])
         ;
     }
     
@@ -26,7 +27,8 @@ class EnterpriseType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'CoreBundle\Entity\Enterprise'
+            'data_class' => 'CoreBundle\Entity\Enterprise',
+            'translation_domain' => 'form'
         ));
     }
 }
